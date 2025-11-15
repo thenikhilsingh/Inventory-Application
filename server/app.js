ea226debaec321cb1dcc007d95dcded90159a4bb
thenikhilsingh <nikhilsingh26042004@gmail.com>
2025-11-15 12:16:17 +0530

    done

--- server/app.js
+++ server/app.js
@@ -14,7 +14,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: "*",
+    origin: "https://inventory-application-five.vercel.app/",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
