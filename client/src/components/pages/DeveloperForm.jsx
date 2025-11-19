30f31451d4856f9b9b70ff40f840436203563fd3
thenikhilsingh <nikhilsingh26042004@gmail.com>
2025-11-19 14:55:26 +0530

    done

--- client/src/components/pages/DeveloperForm.jsx
+++ client/src/components/pages/DeveloperForm.jsx
@@ -146,6 +146,19 @@ export function DeveloperForm() {
             </LabelInputContainer>
           </div>
 
+          {id && (
+            <LabelInputContainer className="mb-8">
+              <Label htmlFor="password">Password</Label>
+              <Input
+                id="password"
+                name="password"
+                placeholder="enter the Admin's Password"
+                type="password"
+                value={developerData.password}
+                onChange={handleChange}
+              />
+            </LabelInputContainer>
+          )}
           <div className="flex gap-5">
             <button
               className="group/btn relative block h-10 w-full rounded-md bg-linear-to-br from-black to-neutral-600 font-medium text-white shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:bg-zinc-800 dark:from-zinc-900 dark:to-zinc-900 dark:shadow-[0px_1px_0px_0px_#27272a_inset,0px_-1px_0px_0px_#27272a_inset]"
